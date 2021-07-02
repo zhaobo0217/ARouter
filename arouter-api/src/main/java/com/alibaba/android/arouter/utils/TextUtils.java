acf94713eb71078c16439d8558d6f9add36dfa62
zhaobo <zhaobo0303@163.com>
2021-07-02 15:40:30 +0800

    修改uri获取参数的方式

--- arouter-api/src/main/java/com/alibaba/android/arouter/utils/TextUtils.java
+++ arouter-api/src/main/java/com/alibaba/android/arouter/utils/TextUtils.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Text utils
@@ -33,38 +34,22 @@ public class TextUtils {
 
     /**
      * Split query parameters
+     *
      * @param rawUri raw uri
      * @return map with params
      */
     public static Map<String, String> splitQueryParameters(Uri rawUri) {
-        String query = rawUri.getEncodedQuery();
-
-        if (query == null) {
+        Set<String> parameterName = rawUri.getQueryParameterNames();
+        if (parameterName == null) {
             return Collections.emptyMap();
         }
-
         Map<String, String> paramMap = new LinkedHashMap<>();
-        int start = 0;
-        do {
-            int next = query.indexOf('&', start);
-            int end = (next == -1) ? query.length() : next;
-
-            int separator = query.indexOf('=', start);
-            if (separator > end || separator == -1) {
-                separator = end;
-            }
-
-            String name = query.substring(start, separator);
-
-            if (!android.text.TextUtils.isEmpty(name)) {
-                String value = (separator == end ? "" : query.substring(separator + 1, end));
-                paramMap.put(Uri.decode(name), Uri.decode(value));
-            }
-
-            // Move start to end of name.
-            start = end + 1;
-        } while (start < query.length());
-
+        for (String key : parameterName) {
+            if (key == null) continue;
+            String value = rawUri.getQueryParameter(key);
+            if (value == null) continue;
+            paramMap.put(key, value);
+        }
         return Collections.unmodifiableMap(paramMap);
     }
 
